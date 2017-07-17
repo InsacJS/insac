c306c692257e76da8a574c35f2ecf7af1f8cdd2e
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2017-07-17 19:38:01 -0400

    Estructura del archivo de rutas actualizada

--- examples/example_01/app.js
+++ examples/example_01/app.js
@@ -7,20 +7,20 @@ app.serverConfig({publicFolder:path.join(__dirname,'public')});
 app.databaseConfig({username:'postgres', password:'BK8DJ567F0', dbname:'insac_example_01'});
 
 app.model({name:'autor', pluralName:'autores', fields:[
-  'nombre',
+  {name:'nombre'},
   {name:'seudonimo', validators:[{name:'len', args:[2,10]}]},
   {name:'nacionalidad', validators:[{name:'isIn', args:[['ARGENTINA', 'BRASIL', 'BOLIVIA', 'ECUADOR']]}], default:'BOLIVIA'}
 ]});
 
-/*app.resource({modelName:'autor', routes:[
+app.resource({modelName:'autor', routes:[
   {method:'GET', outputs:['id', 'nombre', 'seudonimo', 'nacionalidad']},
   {method:'GET', outputs:['nombre', 'seudonimo', 'nacionalidad'], idParam:true},
   {method:'POST', inputs:['nombre', 'seudonimo']},
   {method:'PUT'},
   {method:'DELETE'}
-]});*/
+]});
 
-app.resource('autor');
+//app.resource('autor');
 
 /*app.resource({modelName:'autor', version:2, routes:[
 {method:'POST', inputs:['nombre', 'seudonimo']}
