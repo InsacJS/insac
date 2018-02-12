540f32637a48a9e146c2e95392756e8f2a02df19
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-02-11 20:00:34 -0400

    Se actualizaron las rutas.

--- example/app/src/modules/API/routes/curso/curso.route.js
+++ example/app/src/modules/API/routes/curso/curso.route.js
@@ -2,16 +2,16 @@ module.exports = (router) => {
   router.GET('/api/v1/cursos/', 'listar', {
     description: 'Devuelve una lista de todos los cursos.'
   })
-  router.GET('/api/v1/cursos/:id', 'obtener', {
+  router.GET('/api/v1/cursos/:id_curso', 'obtener', {
     description: 'Devuelve los datos de un curso a partir de su ID.'
   })
   router.POST('/api/v1/cursos/', 'crear', {
     description: 'Crea un nuevo curso.'
   })
-  router.PUT('/api/v1/cursos/:id', 'actualizar', {
+  router.PUT('/api/v1/cursos/:id_curso', 'actualizar', {
     description: 'Actualiza los datos de un curso a partir de su ID.'
   })
-  router.DELETE('/api/v1/cursos/:id', 'eliminar', {
+  router.DELETE('/api/v1/cursos/:id_curso', 'eliminar', {
     description: 'Elimina un curso a partir de su ID.'
   })
 }
