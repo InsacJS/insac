e86ff1f3f5d8f69d131f5b83679c72ee978548b8
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-02-14 20:27:53 -0400

    Se actualizó la definición de los middlewares y los controladores.

--- example/app/src/modules/API/routes/curso/curso.controller.js
+++ example/app/src/modules/API/routes/curso/curso.controller.js
@@ -1,46 +1,46 @@
 module.exports = (app) => {
   const CONTROLLER = {}
 
-  CONTROLLER.listar = async (req, success) => {
+  CONTROLLER.listar = async (req, res) => {
     const OPTIONS = req.options
     const RESULT = await app.DB.sequelize.transaction(async () => {
       return app.DAO.curso.listar(OPTIONS)
     })
-    success(RESULT.rows, 'La lista de cursos ha sido obtenida con éxito.', RESULT.count)
+    return res.success200(RESULT.rows, 'La lista de cursos ha sido obtenida con éxito.', { count: RESULT.count })
   }
 
-  CONTROLLER.obtener = async (req, success) => {
+  CONTROLLER.obtener = async (req, res) => {
     const OPTIONS = req.options
     OPTIONS.where = { id_curso: req.params.id_curso }
     const RESULT = await app.DB.sequelize.transaction(async () => {
       return app.DAO.curso.obtener(OPTIONS)
     })
-    success(RESULT, 'La información del curso ha sido obtenida con éxito.')
+    return res.success200(RESULT, 'La información del curso ha sido obtenida con éxito.')
   }
 
-  CONTROLLER.crear = async (req, success) => {
+  CONTROLLER.crear = async (req, res) => {
     const CURSO = req.body
     const RESULT = await app.DB.sequelize.transaction(async () => {
       return app.DAO.curso.crear(CURSO)
     })
-    success(RESULT, 'El curso ha sido creado con éxito.')
+    return res.success201(RESULT, 'El curso ha sido creado con éxito.')
   }
 
-  CONTROLLER.actualizar = async (req, success) => {
+  CONTROLLER.actualizar = async (req, res) => {
     const CURSO = req.body
     const ID_CURSO = req.params.id_curso
     const RESULT = await app.DB.sequelize.transaction(async () => {
       await app.DAO.curso.actualizar(CURSO, ID_CURSO)
     })
-    success(RESULT, 'Los datos del curso han sido actualizados con éxito.')
+    return res.success200(RESULT, 'Los datos del curso han sido actualizados con éxito.')
   }
 
-  CONTROLLER.eliminar = async (req, success) => {
+  CONTROLLER.eliminar = async (req, res) => {
     const ID_CURSO = req.params.id_curso
     const RESULT = await app.DB.sequelize.transaction(async () => {
       return app.DAO.curso.eliminar(ID_CURSO)
     })
-    success(RESULT, 'Los datos del curso han sido eliminados con éxito.')
+    return res.success200(RESULT, 'Los datos del curso han sido eliminados con éxito.')
   }
 
   return CONTROLLER
--- example/app/src/modules/API/routes/curso/curso.middleware.js
+++ example/app/src/modules/API/routes/curso/curso.middleware.js
@@ -1,47 +1,51 @@
-const { errors } = require(global.INSAC)
-
 module.exports = (app) => {
   const MIDDLEWARE = {}
 
-  MIDDLEWARE.listar = async (req) => {
-    // await app.AUTH.access('ADMIN', req)
-  }
+  MIDDLEWARE.listar = []
 
-  MIDDLEWARE.obtener = async (req) => {
-    const ID_CURSO = req.params.id_curso
-    if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
-      throw new errors.PreconditionFailedError(`No se encuentra el registro del curso solicitado.`)
+  MIDDLEWARE.obtener = [
+    async (req, res) => {
+      const ID_CURSO = req.params.id_curso
+      if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
+        return res.error412(`No se encuentra el registro del curso solicitado.`)
+      }
     }
-  }
+  ]
 
-  MIDDLEWARE.crear = async (req) => {
-    const NOMBRE = req.body.nombre
-    if (await app.DAO.curso.buscar({ nombre: NOMBRE })) {
-      throw new errors.PreconditionFailedError(`El nombre del curso ya se encuentra registrado.`)
+  MIDDLEWARE.crear = [
+    async (req, res) => {
+      const NOMBRE = req.body.nombre
+      if (await app.DAO.curso.buscar({ nombre: NOMBRE })) {
+        return res.error412(`El nombre del curso ya se encuentra registrado.`)
+      }
     }
-  }
+  ]
 
-  MIDDLEWARE.actualizar = async (req) => {
-    const NOMBRE = req.body.nombre
-    const CATEGORIA = req.body.categoria
-    const ID_CURSO = req.params.id_curso
-    if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
-      throw new errors.PreconditionFailedError(`No se encuentra el registro del curso que desea actualizar.`)
+  MIDDLEWARE.actualizar = [
+    async (req, res) => {
+      const NOMBRE = req.body.nombre
+      const CATEGORIA = req.body.categoria
+      const ID_CURSO = req.params.id_curso
+      if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
+        return res.error412(`No se encuentra el registro del curso que desea actualizar.`)
+      }
+      if (!NOMBRE && !CATEGORIA) {
+        return res.error412(`Debe enviar al menos un dato válido, para actualizar el registro del curso.`)
+      }
+      if (NOMBRE && await app.DAO.curso.buscar({ nombre: NOMBRE }, { id_curso: ID_CURSO })) {
+        return res.error412(`El nombre del curso ya se encuentra registrado.`)
+      }
     }
-    if (!NOMBRE && !CATEGORIA) {
-      throw new errors.PreconditionFailedError(`Debe enviar al menos un dato válido, para actualizar el registro del curso.`)
-    }
-    if (NOMBRE && await app.DAO.curso.buscar({ nombre: NOMBRE }, { id_curso: ID_CURSO })) {
-      throw new errors.PreconditionFailedError(`El nombre del curso ya se encuentra registrado.`)
-    }
-  }
+  ]
 
-  MIDDLEWARE.eliminar = async (req) => {
-    const ID_CURSO = req.params.id_curso
-    if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
-      throw new errors.PreconditionFailedError(`No se encuentra el registro del curso que desea eliminar.`)
+  MIDDLEWARE.eliminar = [
+    async (req, res) => {
+      const ID_CURSO = req.params.id_curso
+      if (!await app.DAO.curso.buscar({ id_curso: ID_CURSO })) {
+        return res.error412(`No se encuentra el registro del curso que desea eliminar.`)
+      }
     }
-  }
+  ]
 
   return MIDDLEWARE
 }
