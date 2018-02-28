d162166fe2343e1912b2d9bdc0e07f600944ae84
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-02-28 16:00:10 -0400

    Estructura actualizada.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+/*!
+* insac
+* Copyright(c) 2018 Alex Quispe
+* MIT License
+*/
 module.exports = {
-  Insac: require('./lib/Insac'),
-  AuthModule: require('./lib/modules/AuthModule/AuthModule'),
-  ResourceModule: require('./lib/modules/ResourceModule/ResourceModule'),
-  Field: require('insac-field').Field,
-  FieldContainer: require('insac-field').FieldContainer,
-  THIS: require('insac-field').FieldContainer.THIS,
-  errors: require('insac-response').errors
+  Insac          : require('./lib/Insac'),
+  util           : require('./lib/tools/util'),
+  logger         : require('./lib/tools/logger'),
+  Dao            : require('./lib/tools/Dao'),
+  Module         : require('./lib/modules/Module'),
+  ResourceModule : require('./lib/modules/ResourceModule/ResourceModule'),
+  Validator      : require('input-data-validator').Validator,
+  successes      : require('response-handler').successes,
+  Response       : require('response-handler').Response,
+  Options        : require('sequelize-options').Options,
+  Apidoc         : require('apidoc-creator').Apidoc,
+  errors         : require('response-handler').errors,
+  Field          : require('field-creator').Field,
+  THIS           : require('field-creator').THIS,
+  Seed           : require('seed-creator')
 }
