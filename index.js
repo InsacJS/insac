90f562c9be412b96f69c6a34f859ed3d15347332
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-03-16 17:56:19 -0400

    Se adiciona el módulo email y la opción para configurar opciones avanzadas, incluido el modo seguro (HTTPS)

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 * MIT License
 */
 module.exports = {
-  Insac               : require('./lib/Insac'),
-  util                : require('./lib/tools/util'),
-  logger              : require('./lib/tools/logger'),
-  Dao                 : require('./lib/tools/Dao'),
-  Module              : require('./lib/modules/Module'),
-  ResourceModule      : require('./lib/modules/ResourceModule'),
-  EmailSendGridModule : require('./lib/modules/EmailSendGridModule'),
-  Validator           : require('input-data-validator').Validator,
-  successes           : require('response-handler').successes,
-  Response            : require('response-handler').Response,
-  Options             : require('sequelize-options').Options,
-  Apidoc              : require('apidoc-creator').Apidoc,
-  errors              : require('response-handler').errors,
-  Field               : require('field-creator').Field,
-  THIS                : require('field-creator').THIS,
-  Seed                : require('seed-creator')
+  Insac              : require('./lib/Insac'),
+  util               : require('./lib/tools/util'),
+  logger             : require('./lib/tools/logger'),
+  Dao                : require('./lib/tools/Dao'),
+  Module             : require('./lib/modules/Module'),
+  ResourceModule     : require('./lib/modules/ResourceModule'),
+  SendGridMailModule : require('./lib/modules/SendGridMailModule'),
+  Validator          : require('input-data-validator').Validator,
+  successes          : require('response-handler').successes,
+  Response           : require('response-handler').Response,
+  errors             : require('response-handler').errors,
+  Options            : require('sequelize-options').Options,
+  Apidoc             : require('apidoc-creator').Apidoc,
+  Field              : require('field-creator').Field,
+  THIS               : require('field-creator').THIS,
+  Seed               : require('seed-creator')
 }
