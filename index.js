0160dd08ce1cc94eb6b34ed0b66b8879d03354a1
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-03-29 22:17:39 -0400

    Se actualizó la documentación y se optimizó el código.

--- index.js
+++ index.js
@@ -18,7 +18,27 @@ module.exports = {
   errors             : require('response-handler').errors,
   Options            : require('sequelize-options').Options,
   Apidoc             : require('apidoc-creator').Apidoc,
-  Field              : require('field-creator').Field,
+  Field              : require('./lib/tools/Field'),
   THIS               : require('field-creator').THIS,
   Seed               : require('seed-creator')
 }
+
+// const { Insac }    = require('@insac/core/Insac')
+// const { Module }   = require('@insac/core/Module')
+// const { Response } = require('@insac/core/Response')
+//
+// const { Field } = require('@insac/core/Field')
+// const { Dao }   = require('@insac/core/Dao')
+// const { Seed }  = require('@insac/core/Seed')
+//
+//
+// // const { Apidoc } = require('@insac/core')
+// // const { Database } = require('@insac/core')
+// // const { Response } = require('@insac/core')
+// // const { Validator } = require('@insac/core')
+//
+// const { ResourceModule } = require('@insac/modules')
+//
+// const { util } = require('@insac/tools/util')
+// const { logger } = require('@insac/tools/logger')
+// const { errors } = require('@insac/tools/errors')
