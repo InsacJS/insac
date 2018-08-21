4074fed8d0a227ea8bc9c8206c7eff1fe8ad22cb
Alex Quispe <q.wilmer.alex@gmail.com>
2018-08-21 16:24:45 -0400

    Se actualizó la configuración de las rutas, se incorpora la ruta SOURCES

--- test/integration/2. insac/academico/src/config/path.config.js
+++ test/integration/2. insac/academico/src/config/path.config.js
@@ -4,13 +4,9 @@ const PROJECT_PATH = process.env.PROJECT_PATH
 
 const PATH = {
   project : PROJECT_PATH,
-  modules : path.resolve(PROJECT_PATH, 'src/modules'),
-  config  : path.resolve(PROJECT_PATH, 'src/config'),
-  hooks   : path.resolve(PROJECT_PATH, 'src/hooks'),
-  tools   : path.resolve(PROJECT_PATH, 'src/tools'),
+  sources : path.resolve(PROJECT_PATH, 'src'),
   logs    : path.resolve(PROJECT_PATH, 'logs'),
-  public  : path.resolve(PROJECT_PATH, 'public'),
-  apidoc  : path.resolve(PROJECT_PATH, 'public/apidoc')
+  public  : path.resolve(PROJECT_PATH, 'public')
 }
 
 module.exports = PATH
